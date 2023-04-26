ff49355aab77d01e7f8c367967b14c89c12c4eb6
olivercorrea <116691853+olivercorrea@users.noreply.github.com>
2023-04-26 15:30:55 -0500

    Add files via upload

---
+++ TiposDeDatos.js
@@ -0,0 +1,13 @@
+
+var nombre = "Carlos";
+console.log(nombre);
+
+var numero = 1000;
+console.log(numero);
+
+var objeto = {
+    nombre : "Juan",
+    apellido : "Perez",
+    telefono : "55443322"
+}
+console.log(objeto)
