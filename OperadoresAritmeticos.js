da4899c176adf67890d929b986e7b6894a46dd50
olivercorrea <116691853+olivercorrea@users.noreply.github.com>
2023-04-26 15:30:27 -0500

    Add files via upload

---
+++ OperadoresAritmeticos.js
@@ -0,0 +1,21 @@
+let a = 3, b = 2;
+let z = a + b;
+console.log("Resultado de la suma: " + z);
+
+z = a - b;
+console.log("Resultado de la resta: " + z);
+
+z = a * b;
+console.log("Resultado de la multiplicación: " + z);
+
+z = a / b;
+console.log("Resultado de la división: " + z);
+
+z = a % b;
+console.log("Resultado de operacion (residuo): " + z);
+
+z = a ** b;
+console.log("Resultado de operador exponente: " + z);
+
+
+
